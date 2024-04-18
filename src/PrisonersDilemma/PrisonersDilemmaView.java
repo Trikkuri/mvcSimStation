10b8255229cce163497fb5453632bef50eb1a028
Trikkuri <bpham2018@gmail.com>
2024-04-17 18:14:00 -0700

    Finished PDSim

--- src/PrisonersDilemma/PrisonersDilemmaView.java
+++ src/PrisonersDilemma/PrisonersDilemmaView.java
@@ -7,34 +7,49 @@ import java.util.Iterator;
 
 public class PrisonersDilemmaView extends SimulationView {
 
+    private final Color COLOR_COOPERATE = Color.GREEN;
+    private final Color COLOR_RANDOMLY_COOPERATE = Color.YELLOW;
+    private final Color COLOR_CHEAT = Color.RED;
+    private final Color COLOR_TIT4TAT = Color.BLUE;
+
     public PrisonersDilemmaView(Model model) {
         super(model);
     }
 
     @Override
     protected void drawAgents(Graphics gc) {
-        Simulation simulation = (Simulation) model;
-        Iterator<Agent> it = simulation.agentIterator();
+        PrisonersDilemmaSimulation ps = (PrisonersDilemmaSimulation) model;
+        Iterator<Agent> it = ps.agentIterator();
+
+        int centerOffset = AGENT_SIZE / 2;
 
         while (it.hasNext()) {
-            Agent agent = it.next();
-            if (agent instanceof Prisoner) {
-                Prisoner prisoner = (Prisoner) agent;
-                Color color = prisoner.cooperate() ? Color.GREEN : Color.RED;
-                gc.setColor(color);
+            Prisoner p = (Prisoner) it.next();
+            StrategyType type = p.getStrategy().getType();
 
-                int x = prisoner.xc;
-                int y = prisoner.yc;
-                int size = AGENT_SIZE;
-                int halfSize = size / 2;
-                gc.fillOval(x - halfSize, y - halfSize, size, size);
+            switch (type) {
+                case COOPERATE:
+                    gc.setColor(COLOR_COOPERATE);
+                    break;
+                case RANDOMLY_COOPERATE:
+                    gc.setColor(COLOR_RANDOMLY_COOPERATE);
+                    break;
+                case CHEAT:
+                    gc.setColor(COLOR_CHEAT);
+                    break;
+                case TIT4TAT:
+                    gc.setColor(COLOR_TIT4TAT);
+                    break;
             }
+
+            // Draw the agent
+            gc.fillOval(p.xc - centerOffset, p.yc - centerOffset, AGENT_SIZE, AGENT_SIZE);
         }
     }
 
     @Override
     public void paintComponent(Graphics gc) {
-        super.paintComponent(gc); // Ensure the background is cleared
-        drawAgents(gc); // Custom agent drawing
+        super.paintComponent(gc);
+        drawAgents(gc);
     }
 }
